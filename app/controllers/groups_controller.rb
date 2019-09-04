97c678d28a9c79036e2c99155d1ac126d8e1650a
kei-60th <kei-1005@i.softbank.jp>
2019-09-04 13:19:46 +0900

    add action create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,23 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(create_params)
+    redirect_to controller: :posts, action: :index
   end
 
   def edit
   end
 
+
   def update
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name)
   end
 
 end
