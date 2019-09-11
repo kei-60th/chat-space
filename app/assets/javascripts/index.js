d15f30f44f17422da93e28f7b37f78e0302d43c6
kei-60th <kei-1005@i.softbank.jp>
2019-09-11 21:29:03 +0900

    0911_update

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,57 +1,84 @@
 document.addEventListener("turbolinks:load", function() {
 
-  function buildMessage(message){
 
-    if (message.image_url) {
-      image_data = `<img class="lower-message__image" src=${message.image_url} alt="19248 en 1" />`
-    }
-    else {
-      image_data = ``
-    }
 
-    var html = `<div class='message last'>
-                  <div class='message__upper-info'>
-                    <p class='message__upper-info__talker'>
-                      ${message.user_name}
-                    </p>
-                    <p class='message__upper-info__date'>
-                      ${message.created_at}
-                    </p>
-                  </div>
-                  <p class='message__text'>
-                    ${message.body}
-                  </p>
-                    ${image_data}
-                </div>`
-    return html;
+
+
+  var search_list = $("#user-search-result");
+  var add_list = $(".chat-group-users.js-add-user");
+
+  function appendCandidate(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                 </div>`
+      search_list.append(html);
   }
 
-  $('#new_message').on('submit',function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+  function appendGroup(user) {
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                 </div>`
+      search_list.append(html);
+  }
+
+
+
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    search_list.append(html);
+  }
+
+
+
+  $("#user-search-field.chat-group-form__input").on("keyup", function() {
+    var input = $("#user-search-field.chat-group-form__input").val();
+
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      type: 'GET',
+      url: '/users',
+      data: {keyword: input},
+      dataType: 'json'
     })
-    .done(function(message){
-      var html = buildMessage(message);
-      $(".messages").append(html)
-      $('.input-box__text').val('')
-      $('.input-box__image__file').val('')
-      $(".messages").animate({scrollTop:$(".messages")[0].scrollHeight},200)
-      $(".submit-btn").prop("disabled", false);
 
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendCandidate(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
     })
-    .fail(function(){
-      alert('メッセージを入力して下さい');
-      $(".submit-btn").prop("disabled", false);
+    .fail(function() {
+      console.log("NG")
     })
-  })
+  });
+
+  $(document).on("click",".user-search-add.chat-group-user__btn.chat-group-user__btn--add",function(){
+    console.log("OK")
+
+    $.ajax({
+      type: 'GET',
+      url: '/users/new',
+      data: {},
+      dataType: 'json'
+    })
+  });
+
+
+
+
+
+
+
+
+
 });
-
-
